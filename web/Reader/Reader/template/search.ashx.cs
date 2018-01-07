9a30d8c52ffb61499960cc6b08b1ad2c8579e2b5
JuneXdy <xdy7456@163.com>
2018-01-07 15:40:37 +0800

    search.htm

---
+++ web/Reader/Reader/template/search.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reader.template
+{
+    /// <summary>
+    /// index 的摘要说明
+    /// </summary>
+    public class index : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            //context.Response.Write("Hello World");
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
