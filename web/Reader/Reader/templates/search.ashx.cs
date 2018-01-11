d86f1a0d1bdcc106eb34c8bf4e3050fc9435e0fb
JuneXdy <xdy7456@163.com>
2018-01-11 16:50:00 +0800

    完成网页查询功能

--- web/Reader/Reader/templates/search.ashx.cs
+++ web/Reader/Reader/templates/search.ashx.cs
@@ -19,62 +19,92 @@ namespace Reader.template
             context.Response.ContentType = "text/html";
             //context.Response.Write("Hello World");
             string schitem = context.Request["schitem"];
-            //string schtx=context.Request["schtx"];
+            string schtx=context.Request["schtx"];
+            
+            string html = "";
 
             DataTable dt = new DataTable();
 
-            if (string.IsNullOrEmpty(schitem) || string.IsNullOrEmpty(context.Request["schtx"]))//scan all case
+            if (string.IsNullOrEmpty(schitem) || string.IsNullOrEmpty(schtx))//scan all case
             {
                 dt = SqlHelper.ExecuteDataTable("select * from caseBook");
-                var Data = new { book = dt.Rows };
-                string html = CommonHelper.RenderHtml("schCResult.htm", Data);
+                schtx = "";
+                var Data = new
+                {
+                    book = dt.Rows,
+                    tx = "",
+                };
+                html = CommonHelper.RenderHtml("schCResult.htm", Data);
                 context.Response.Write(html);
             }
             else//search
             {
                 string a = "%%";
-                string schtx = a.Insert(1, context.Request["schtx"]);
+                schtx = a.Insert(1, context.Request["schtx"]);
+                
                 if (schitem == "caseName"  )
                 {
-                    dt = SqlHelper.ExecuteDataTable("select * from caseBook where caseName like @tx",
-                       new SqlParameter("@tx", schtx));
-                    var Data = new { book = dt.Rows };
-                    string html = CommonHelper.RenderHtml("schCResult.htm", Data);
+                    dt = SqlHelper.ExecuteDataTable("select * from  caseBook  where caseName like @tx",new SqlParameter("@tx",schtx));
+                    //allnum = SqlHelper.ExecuteDataTable("select * from caseBook where caseName like @tx",new SqlParameter("@tx",schtx)).Rows.Count;
+                    var Data = new
+                    {
+                        book = dt.Rows,
+                        tx = context.Request["schtx"],
+                    };
+                    html = CommonHelper.RenderHtml("schCResult.htm", Data);
                     context.Response.Write(html);
                 }
                 else if (schitem == "isbn") 
                 {
-                    dt = SqlHelper.ExecuteDataTable("select * from caseBook where isbn like @tx",
-                       new SqlParameter("@tx", schtx));
-                    var Data = new { book = dt.Rows };
-                    string html = CommonHelper.RenderHtml("schCResult.htm", Data);
+                    dt = SqlHelper.ExecuteDataTable("select * from caseBook where isbn like @tx",new SqlParameter("@tx",schtx));
+                    //allnum = SqlHelper.ExecuteDataTable("select * from caseBook where isbn like @tx",new SqlParameter("@tx",schtx)).Rows.Count;
+                    var Data = new
+                    {
+                        book = dt.Rows,
+                        tx = context.Request["schtx"],
+                    };
+                    html = CommonHelper.RenderHtml("schCResult.htm", Data);
                     context.Response.Write(html);
                 }
                 else if (schitem == "writer")
                 {
-                    dt = SqlHelper.ExecuteDataTable("select * from caseBook where writer like @tx",
-                       new SqlParameter("@tx", schtx));
-                    var Data = new { book = dt.Rows };
-                    string html = CommonHelper.RenderHtml("schCResult.htm", Data);
+                    dt = SqlHelper.ExecuteDataTable("select * from caseBook where writer like @tx",new SqlParameter("@tx",schtx));
+                    //allnum = SqlHelper.ExecuteDataTable("select * from caseBook where writer like @tx", new SqlParameter("@tx", schtx)).Rows.Count;
+                    var Data = new
+                    {
+                        book = dt.Rows,
+                        tx = context.Request["schtx"],
+                    };
+                    html = CommonHelper.RenderHtml("schCResult.htm", Data);
                     context.Response.Write(html);
                 }
                 else if (schitem == "vwriter")
                 {
-                    dt = SqlHelper.ExecuteDataTable("select * from vsch where vwriter like @tx",
-                       new SqlParameter("@item", schitem), new SqlParameter("@tx", schtx));
-                    var Data = new { book = dt.Rows };
-                    string html = CommonHelper.RenderHtml("schVResult.htm", Data);
+                    dt = SqlHelper.ExecuteDataTable("select * from vsch where vwriter like @tx", new SqlParameter("@tx", schtx));
+                    
+                    var Data = new
+                    {
+                        book = dt.Rows,
+                        tx = context.Request["schtx"],
+                    };
+                    html = CommonHelper.RenderHtml("schVResult.htm", Data);
                     context.Response.Write(html);
                 }
-                else if (schitem == "vname")
+                else if (schitem == "division")
                 {
-                    dt = SqlHelper.ExecuteDataTable("select * from vsch where vname like @tx",
-                       new SqlParameter("@item", schitem), new SqlParameter("@tx", schtx));
-                    var Data = new { book = dt.Rows };
-                    string html = CommonHelper.RenderHtml("schVResult.htm", Data);
+                    dt = SqlHelper.ExecuteDataTable("select * from vsch where division like @tx", new SqlParameter("@tx", schtx));
+                    
+                    var Data = new
+                    {
+                        book = dt.Rows,
+                        tx = context.Request["schtx"],
+                    };
+                    html = CommonHelper.RenderHtml("schVResult.htm", Data);
                     context.Response.Write(html);
                 }
             }
+            
+
         }
 
         public bool IsReusable
